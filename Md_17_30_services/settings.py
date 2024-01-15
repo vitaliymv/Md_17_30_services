379a069747a5983c32b09effa7d423132b6da744
vitaliymv <movcan77@gmail.com>
2024-01-15 18:18:23 +0200

    Disable debug, update allowed hosts

--- Md_17_30_services/settings.py
+++ Md_17_30_services/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nh#r*e6)%+mtx^+&^b1s3_#@k$g7dham)p88_6f+^y0k(qrf*f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
